49332296f2d7c3bc3c131912241f7d9d1eb56fcc
Mussclik <xipsmw@gmail.com>
2023-10-22 22:00:32 +0200

    gauh

--- bugBattles/Assets/Scripts/heart of the cards/redo it/Components/Component.cs
+++ bugBattles/Assets/Scripts/heart of the cards/redo it/Components/Component.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class Component : MonoBehaviour
 {
+
+
+    public delegate void Deligate();
+    public Deligate buh;
+    protected event Deligate turnStart, turnAdvance, turnEnd;
+
+
+
     void Start()
     {
-        
+        buh = null;
+        buh += Method();
+    }
+
+    void Method()
+    {
+
     }
 
     void Update()
     {
         
+    }
+
+    internal void TurnStart()
+    {
+        turnStart();
+    }
+
+    internal void TurnAdvance()
+    {
+        turnAdvance();
+    }
+
+    internal void TurnEnd()
+    {
+        turnEnd();
     }
 }
